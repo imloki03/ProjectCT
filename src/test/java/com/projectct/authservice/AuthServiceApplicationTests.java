ceb69aa0f46fb0366c04d3cfa0e46e7487d83e48
Nguyen Van Thi <xc.lokigod@gmail.com>
2024-12-21 19:22:12 +0700

    Init commit

---
+++ src/test/java/com/projectct/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectct.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
